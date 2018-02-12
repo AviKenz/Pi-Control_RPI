6355e10077c8e27ea7ccee85767326f6dbbc08ba
AviKenz <kennefoue@gmail.com>
2018-02-12 22:44:51 +0000

    log funktion optimiert und enthält datum und uhrzeit jetzt

--- PiControlLib.py
+++ PiControlLib.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python
 
 import sys
+import RPi.GPIO as GPIO
+import datetime
+import time
 
-initialized = False
 args = sys.argv
-	
-def init(pArgs):
-	args = pArgs
-	initialized = True
-	print "INIT.... after {}".format(initialized)
 
 def log(code, message):
+	now = datetime.datetime.now()
+	now = now.strftime("%Y-%m-%d %H:%M:%S")
+	clName = ""
 	result = "";
 	if(code == "e"):
-		result = "<p class='interpreter error'>" + message + "</p>"
+		clName = 'interpreter error'
 	elif(code == "w"):
-		result = "<p class='interpreter warn'>" + message + "</p>"
+		clName = 'interpreter warn'
 	elif(code == "i"):
-		result = "<p class='interpreter info'>" + message + "</p>"
+		clName = 'interpreter info'
 	elif(code == "d"):
-		result = "<p class='interpreter debug'>" + message + "</p>"
+		clName = 'interpreter debug'
 	elif(code == "t"):
-		result = "<p class='interpreter todo'>" + message + "</p>"
+		clName = 'interpreter todo'
 	else:
-		result = "<p class='interpreter noname'>" + message + "</p>"
+		clName = 'interpreter noname'
+	result = "<span class=" + clName + ">" + now + " " + message + "</p>"
 	print result
 	
 # get value of key in paramter passed to the py script
-def get(key, isRequired = False, isNumber = True):
+def get(key, isRequired = False, isString = False):
 	log("d", "get({})".format(key))
 	i = 0
 	find = False
@@ -36,8 +37,9 @@ def get(key, isRequired = False, isNumber = True):
 		if(pair.find(key) != -1):
 			find = True
 			val = pair.split("=")[1]
-			if( isNumber ):
-				val = int(val)
+			val = int(val)
+			if( isString ):
+				val = str(val)
 	if(find == False):
 		log("w", "value of {} not found".format(key))
 		if( isRequired ):
@@ -46,3 +48,54 @@ def get(key, isRequired = False, isNumber = True):
 	else:
 		log("d", "key: {} - value: {}".format(key, val))
 	return val
+
+# output HIGH or LOW to choosen GPIO
+def outputState(pinNr, state, release = False):
+	log("d", "outputState({}, {})".format(pinNr, state))
+	GPIO.output(pinNr, state)
+	if(state == GPIO.LOW and release):
+		releaseGPIO(pinNr)
+
+# free GPIO ressources
+def releaseGPIO(pinNr):
+	log("d", "releaseGPIO({})".format(pinNr))
+	GPIO.output(pinNr, GPIO.LOW)
+	GPIO.cleanup()
+
+# output 1 and 0 once to choosen GPIO port; CTRL + C abort the process
+def blinkOnce(pinNr, interval):
+	try:
+		outputState(pinNr, GPIO.HIGH)
+		time.sleep(interval)
+		outputState(pinNr, GPIO.LOW)
+		time.sleep(interval)
+	except KeyboardInterrupt:
+		log("d", "Programm Interrupted with CTRL + C")
+		releaseGPIO(pinNr)
+
+# alternate state on GPIO a couple of times(nbrOfCycles) in some interval of time(interval in s);	
+def blink(pinNr, interval, nbrOfCycles):
+	log("d", "blink({}, {})".format(pinNr, interval))
+	if(nbrOfCycles == 0):
+		while(True):
+			blinkOnce(pinNr, interval)
+	else:
+		n = nbrOfCycles
+		i = 0
+		while(i < n):
+			blinkOnce(pinNr, interval)
+			i += 1
+		releaseGPIO(pinNr)
+
+# globally handle signal output on GPIO
+def handleDcOutput(pinNr, state, interval, nbrOfCycles):
+	log("d", "handleDcOutput({}, {}, {}, {})".format(pinNr, state, interval, nbrOfCycles))	
+	if(interval == 0 or interval == ""):
+		# make sure to release the GPIO when state is GPIO.LOW
+		outputState(pinNr, state, True)
+	else:
+		blink(pinNr, interval, nbrOfCycles)
+
+def handlePwmOutput(ch, freq, dc):
+	log("d", "handlePwmOutput({}, {}, {})".format(ch, freq, dc))		
+	
--- RpiInterpreter.py
+++ RpiInterpreter.py
@@ -15,59 +15,6 @@ GPIO.setwarnings(False)
 # constant
 SIG_TYP_DC = 0
 SIG_TYP_PWM = 1
-
-
-# output HIGH or LOW to choosen GPIO
-def outputState(pinNr, state, release = False):
-	AppLib.log("d", "outputState({}, {})".format(pinNr, state))
-	GPIO.output(pinNr, state)
-	if(state == GPIO.LOW and release):
-		releaseGPIO(pinNr)
-
-# free GPIO ressources
-def releaseGPIO(pinNr):
-	AppLib.log("d", "releaseGPIO({})".format(pinNr))
-	GPIO.output(pinNr, GPIO.LOW)
-	GPIO.cleanup()
-
-# output 1 and 0 once to choosen GPIO port; CTRL + C abort the process
-def blinkOnce(pinNr, interval):
-	try:
-		outputState(pinNr, GPIO.HIGH)
-		time.sleep(interval)
-		outputState(pinNr, GPIO.LOW)
-		time.sleep(interval)
-	except KeyboardInterrupt:
-		AppLib.log("d", "Programm Interrupted with CTRL + C")
-		releaseGPIO(pinNr)
-
-# alternate state on GPIO a couple of times(nbrOfCycles) in some interval of time(interval in s);	
-def blink(pinNr, interval, nbrOfCycles):
-	AppLib.log("d", "blink({}, {})".format(pinNr, interval))
-	if(nbrOfCycles == 0):
-		while(True):
-			blinkOnce(pinNr, interval)
-	else:
-		n = nbrOfCycles
-		i = 0
-		while(i < n):
-			blinkOnce(pinNr, interval)
-			i += 1
-		releaseGPIO(pinNr)
-
-# globally handle signal output on GPIO
-def handleDcOutput(pinNr, state, interval, nbrOfCycles):
-	AppLib.log("d", "handleDcOutput({}, {}, {}, {})".format(pinNr, state, interval, nbrOfCycles))	
-	if(interval == 0 or interval == ""):
-		# make sure to release the GPIO when state is GPIO.LOW
-		outputState(pinNr, state, True)
-	else:
-		blink(pinNr, interval, nbrOfCycles)
-
-def handlePwmOutput(ch, freq, dc):
-	AppLib.log("d", "handlePwmOutput({}, {}, {})".format(ch, freq, dc))		
-	
-
 									
 # handle params passed to .py script an decide the action to take
 def handleParam():
@@ -84,7 +31,7 @@ def handleParam():
 			state = AppLib.get("state")
 			interval = AppLib.get("interval")
 			nbrOfCycles = AppLib.get("number_of_cycles")
-			handleDcOutput(pinNr, state, interval, nbrOfCycles)
+			AppLib.handleDcOutput(pinNr, state, interval, nbrOfCycles)
 		elif(signalTyp == SIG_TYP_PWM):
 			AppLib.log("t", "implementing GPIO-OUTPUT-PWM... !")
 			AppLib.log("d", "channel = pin_number = {}".format(pinNr))
